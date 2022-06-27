9a58ead30459641e9ba7cdf1d4ebcad9d94899d4
Dennis Li <dennisli.vip@gmail.com>
2022-06-27 11:26:48 -0400

    Update

--- summer2022/IDL.py
+++ summer2022/IDL.py
@@ -6,33 +6,34 @@ from pytipsy import rtipsy  #reads
 
 file = '/mnt/data0/jillian/ICS/ICInG/IsolatedICsFromMichael/DMOnlyCollapse_5e11_8388608_Physical_0.01.000480'
 file2 = '/mnt/data0/jillian/ICS/ICInG/IsolatedICsFromMichael/DMOnlyCollapse_5e11_8388608_Physical_0.01_BH2.000480'
+file3 = '/mnt/data0/jillian/ICS/ICInG/IsolatedICsFromMichael/DMOnlyCollapse_5e11_8388608_Physical_0.01_BH3.000480'
 
 h, g, d, s = rtipsy(file)
 
 munit = 1.84793E16
-mass = (1E7)/munit
-x = 0.0
-y = 0.0
-z = 0.0
-vx = 0.0
-vy = 0.0
-vz = 0.0
-phi = 0.0
-eps = 3.11226E-06
-tform = -1.0
-metals = 0.0
+mass = [((1E7)/munit), ((1E7)/munit)]
+x = ['0.0', '3.11226E-06']
+y = ['0.0', '0.0']
+z = ['0.0', '0.0']
+vx = ['0.0', '0.0']
+vy = ['0.0', '0.0']
+vz = ['0.0', '0.0']
+phi = ['0.0', '0.0']
+eps = ['3.11226E-06', '3.11226E-06']
+tform = ['-1.0', '-1.0']
+metals = ['0.0', '0.0']
 
 s = {'mass':mass, 'pos':[x,y,z], 'vel':[vx,vy,vz], 'metals':metals, 'tform':tform, 'eps':eps, 'phi':phi}
 header = {'time':h['time'], 'n':h['n'] + 1, 'ndim':h['ndim'], 'ngas':h['ngas'], 'ndark':h['ndark'], 'nstar':h['nstar'] + 1}
 print("writing file")
-wtipsy(file2, h, g, d, s)
+#wtipsy(file2, h, g, d, s)
+wtipsy(file3, h, g, d, s)
+
+
+
 
 
 '''
-f = open(file2, 'rb')
-fs = len(f.read())
-t, n, ndim, ng, nd, ns = struct.unpack("<fiiiii", f.read(24))
-
 f = pynbody.load(file2) #loads the file 
 f.properties
  
@@ -42,26 +43,4 @@ f['eps'][8388608]
 f['phi'][8388608]
 f['mass'][8388608]
 f['pos'][8388608]
-'''
-
-'''
-array = pynbody.array.SimArray(np.array(file2[0]))
-array.sim = f
-array
-
-#h = f.header
-d = f.dark
-#g = f.gas
-s = f.star
-'''
-
-
-
-'''
-h.nstar = 1L #long integer 
-h.n = h.n + 1L
-
-
-wtipsy('DMOnlyCollapse_5e11_8388608_Physical_0.01_BH.000480')
-
 '''
