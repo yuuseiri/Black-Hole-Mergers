2569d1dd176e83c51b2ca91228f758551f81f9df
Dennis Li <dennisli.vip@gmail.com>
2022-06-28 13:55:22 -0400

    looped

--- summer2022/test1data.py
+++ summer2022/test1data.py
@@ -6,12 +6,12 @@ directory = '/mnt/data0/jillian/ICS/ICInG/IsolatedICsFromMichael/test1/'
 
 for filename in os.listdir(directory):
     f = os.path.join(directory, filename)
-    if os.path.isfile(f) and (filename.endswith(".param") == False):
+    if os.path.isfile(f) and (filename.endswith(".param") == False) and (filename.endswith(".txt") == False):
         i = pynbody.load(f)
-        print(i.stars['pos'])
         print(i.stars['vel'])
+        print(i.stars['pos'])
     else: 
-        print("There was a .param file")
+        print("There was a .param or .txt file")
         
         
         
