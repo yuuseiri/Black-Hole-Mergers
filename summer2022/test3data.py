e5087d9f08aaf25c978ff9085df1582d74b33084
Dennis Li <dennisli.vip@gmail.com>
2022-07-19 11:12:08 -0400

    k

--- summer2022/test3data.py
+++
@@ -1,7 +0,0 @@
-import pynbody 
-import os
-import numpy as np
-import matplotlib.pyplot as plt 
-from mpl_toolkits import mplot3d
-
-directory3 = '/mnt/data0/jillian/ICS/ICInG/IsolatedICsFromMichael/test3/DMOnlyCollapse_test1.000016'
