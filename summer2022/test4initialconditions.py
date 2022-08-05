4875b68a4aee458b56b5009f5f3b3b7784677ab3
Dennis Li <dennisli.vip@gmail.com>
2022-08-05 10:27:24 -0400

    new stuff

---
+++ summer2022/test4initialconditions.py
@@ -0,0 +1,30 @@
+import pynbody
+import numpy as np
+import struct
+from pytipsy import wtipsy 
+from pytipsy import rtipsy  
+
+file = '/mnt/data0/jillian/ICS/ICInG/IsolatedICsFromMichael/DMOnlyCollapse_5e11_8388608_Physical_0.01.000480'
+file4 = '/mnt/data0/jillian/ICS/ICInG/IsolatedICsFromMichael/DMOnlyCollapse_5e11_8388608_Physical_0.01_BH4.000480'
+
+h, g, d, s = rtipsy(file)
+
+softening = 3.11226E-06
+
+munit = 1.84793E16
+mass = [((1E7)/munit), ((1E7)/munit)]
+x = [0.0, 10*softening] 
+y = [0.0, 0.0]
+z = [0.0, 0.0]
+vx = [0.0, 10/1260] #not in km/s
+vy = [0.0, 0.0]
+vz = [0.0, 0.0]
+phi = [0.0, 0.0]
+eps = [softening, softening]
+tform = [-1.0, -1.0]
+metals = [0.0, 0.0]
+
+s = {'mass':mass, 'x':x, 'y':y, 'z':z, 'vx':vx, 'vy':vy, 'vz':vz, 'metals':metals, 'tform':tform, 'eps':eps, 'phi':phi}
+header = {'time':h['time'], 'n':h['n'] + 2, 'ndim':h['ndim'], 'ngas':h['ngas'], 'ndark':h['ndark'], 'nstar':h['nstar'] + 2} 
+print("writing file")
+wtipsy(file4, header, g, d, s)
